fc01df0e7e9a429f29797e4b97b55ff1a1dbd77d
Chris Milford <panicst8@Chriss-MacBook-Pro.local>
2017-11-05 11:38:41 -0600

    example 5

--- lessons.js
+++ lessons.js
@@ -1,4 +1,8 @@
-// Example 4 -- Observable Timer
+// Example 5 -- Observable Time Interval
 
-const timer = Rx.Observable.timer(1000);
-timer.subscribe(done => print('ding!!!'));
+const interval = Rx.Observable.interval(1000);
+interval.subscribe(i => print(i));
+
+// 0
+// 1
+// every second for eternity...
