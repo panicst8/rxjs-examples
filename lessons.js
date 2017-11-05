a692c0cd95c24ed890ca0718350821cdd267f688
Chris Milford <panicst8@Chriss-MacBook-Pro.local>
2017-11-05 11:35:15 -0600

    example 4

--- lessons.js
+++ lessons.js
@@ -1,19 +1,4 @@
-// Example 3 -- Observable from Promise
+// Example 4 -- Observable Timer
 
-const promise = new Promise((resolve, reject) => {
-   setTimeout(() => {
-      resolve('resolved!');
-   }, 1000);
-});
-const obsvPromise = Rx.Observable.fromPromise(promise);
-obsvPromise.subscribe(result => print(result));
-
-// wail 1 second...
-// resolved!
-
-/*
-
-  You can also convert an observable right back to a promise
-  using the toPromise() method
-
-*/
+const timer = Rx.Observable.timer(1000);
+timer.subscribe(done => print('ding!!!'));
