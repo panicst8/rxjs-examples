70dd5b8f9322b3116e95e7e67d1e8f8f5984ab99
Chris Milford <panicst8@Chriss-MacBook-Pro.local>
2017-11-05 14:34:13 -0600

    example 12

--- lessons.js
+++ lessons.js
@@ -1,5 +1,14 @@
 // Example 12 -- Do - Just do it
 
+/*
+
+ The do operator allows you to run code at any point in the
+ Observable, without producing side effects on the emitted 
+ values. This is handy for debugging or for any situation 
+ where you want to run code outside of the Observable scope.
+
+*/
+
 const names = Rx.Observable.of('Simon', 'Garfunkle');
 names
    .do(name => print(`original value: ${name}`))
