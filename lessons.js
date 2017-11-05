18f70348b8152fad4df4cfdf5486c05ce185639d
Chris Milford <panicst8@Chriss-MacBook-Pro.local>
2017-11-05 16:18:11 -0600

    example 20

--- lessons.js
+++ lessons.js
@@ -1,11 +1,14 @@
 // Example 20 -- TakeUntil - Get values until you’re told not to
 
+// this is a good way to provide an external control
+
 const interval = Rx.Observable.interval(500);
 const notifier = Rx.Observable.timer(2000);
 interval
    .takeUntil(notifier)
    .finally(() => print('Complete!'))
    .subscribe(i => print(i));
+
 // 0
 // 1
 // 2
