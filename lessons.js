41ff89597d00c3744cb08d674a343adffd07f89b
Chris Milford <panicst8@Chriss-MacBook-Pro.local>
2017-11-05 16:09:20 -0600

    example 20

--- lessons.js
+++ lessons.js
@@ -1,8 +1,12 @@
-// Example 19 -- SwitchMap - Get value from Observable A, then emit Observable B
+// Example 20 -- TakeUntil - Get values until you’re told not to
 
-const clicks = Rx.Observable.fromEvent(document, 'click');
-clicks
-   .switchMap(click => {
-      return Rx.Observable.interval(500);
-   })
+const interval = Rx.Observable.interval(500);
+const notifier = Rx.Observable.timer(2000);
+interval
+   .takeUntil(notifier)
+   .finally(() => print('Complete!'))
    .subscribe(i => print(i));
+// 0
+// 1
+// 2
+// Complete!
