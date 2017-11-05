6e6817f23ed073dd981cf741e2b75e117f3bcc9e
Chris Milford <panicst8@Chriss-MacBook-Pro.local>
2017-11-05 15:13:22 -0600

    example 16

--- lessons.js
+++ lessons.js
@@ -1,5 +1,5 @@
-// Example 15 -- first() operator
+// Example 16 -- last() operator
 
 const numbers = Rx.Observable.of(-3, 5, 7, 2, -7, 9, -2);
 
-numbers.first().subscribe(n => print(n));
+numbers.last().subscribe(n => print(n));
