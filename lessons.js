366c724ff421924663631e9ca5c6217843da471a
Chris Milford <panicst8@Chriss-MacBook-Pro.local>
2017-11-05 13:03:50 -0600

    example 7

--- lessons.js
+++ lessons.js
@@ -1,10 +1,35 @@
-// Example 6 -- Observable of Static Values
+// Example 7 -- Unsubscribe - Turn off the stream
 
-const mashup = Rx.Observable.of('anything', ['you', 'want'], 23, true, { cool: 'stuff' });
-mashup.subscribe(val => print(val));
+// Some Observables end on their own
 
-// anything
-// you,want
-// 23
-// true
-// [object Object]
+const timer = Rx.Observable.timer(2000);
+
+timer.finally(() => print('Timer Complete.')).subscribe();
+
+// wait 2 seconds...
+// Timer Complete.
+
+// Some Observables will run forever
+
+const interval = Rx.Observable.interval(1000);
+
+interval.finally(() => print('I will never complete.')).subscribe(x => print(x));
+
+// 0
+// 1
+// and so on...
+
+// You can turn off the stream by unsubscribing
+
+const subscription = interval
+   .finally(x => print('Second Subscribtion Complete.'))
+   .subscribe(x => print('2nd Subscription: ' + x));
+setTimeout(() => {
+   subscription.unsubscribe();
+}, 4000);
+
+// 2nd Subscription: 0
+// 2nd Subscription: 1
+// 2nd Subscription: 2
+
+// Second Subscription Complete.
