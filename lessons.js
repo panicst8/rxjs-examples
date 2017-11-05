df818722e7842a045ca2ff8470dba78156b2deac
Chris Milford <panicst8@Chriss-MacBook-Pro.local>
2017-11-05 13:28:20 -0600

    example 10

--- lessons.js
+++ lessons.js
@@ -1,13 +1,25 @@
-// Example 9 -- Hot Observable Example
+// Example 10 -- Converting Cold Observable to Hot
 
-const x = Math.random();
+/*
 
-const hot = Rx.Observable.create(observer => {
-   observer.next(x);
+ But how do we make an already cold observable hot? We can
+ make a cold Observable hot by simply calling publish() on
+ it. This will allow the subscribers to share the same values
+ in realtime. To make it start emitting values, you call connect()
+ after the subscription has started.
+
+*/
+
+const cold = Rx.Observable.create(observer => {
+   observer.next(Math.random());
 });
+
+const hot = cold.publish();
 
 hot.subscribe(a => print(`Subscriber A: ${a}`));
 hot.subscribe(b => print(`Subscriber B: ${b}`));
 
-// Subscriber A: 0.312580103
-// Subscriber B: 0.312580103
+hot.connect();
+
+/// Subscriber A: 0.7122882102
+/// Subscriber B: 0.7122882102
