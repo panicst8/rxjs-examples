92672aaed7e4e1cddbd010cd4c6f800d0a12e190
Chris Milford <panicst8@Chriss-MacBook-Pro.local>
2017-11-05 22:52:30 -0600

    example 23

--- lessons.js
+++ lessons.js
@@ -1,18 +1,14 @@
-// Example 22 -- Buffer - Collect values, then emit as Array
+// Example 23 -- Zip - Combine in unison
 
-// Buffer is similar to debounce and throttle, but rather than
-// filtering out data, it accumulates it into an array, then emits
-// the array.
+// Zip works well when you have complimentary Observables that
+// you know have an equal number of values. It will combine the
+// values by index location and emit them as a combined array
 
-const mouseEvents = Rx.Observable.fromEvent(document, 'mousemove');
+const yin = Rx.Observable.of('peanut butter', 'wine', 'rainbows');
+const yang = Rx.Observable.of('jelly', 'cheese', 'unicorns');
+const combo = Rx.Observable.zip(yin, yang);
+combo.subscribe(arr => console.log(arr));
 
-mouseEvents
-   .map(e => e.timeStamp.toFixed(0))
-   .bufferTime(1000)
-   .subscribe(batch => console.log(batch));
-// second 1 - [...values]
-// second 2 - [...values]
-
-mouseEvents.bufferCount(5).subscribe(batch => console.log(batch));
-// [...values] length 5
-// [...values] length 5
+// peanut butter, jelly
+// wine, cheese
+// rainbows, unicorns
