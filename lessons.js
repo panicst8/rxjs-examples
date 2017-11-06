02d3b672603071af01695766750f56321118068c
Chris Milford <panicst8@Chriss-MacBook-Pro.local>
2017-11-05 23:11:32 -0600

    example 25

--- lessons.js
+++ lessons.js
@@ -1,16 +1,16 @@
-// Example 24 -- ForkJoin - Merge, wait for completion, emit last values together
+// Example 25 -- Catch - Handle errors gracefully
 
-// The forkJoin() operator is commonly used to combine multiple
-// Promises that must all be resolved before sending data to the UI.
+const observable = Rx.Observable.create(observer => {
+   observer.next('good');
+   observer.next('great');
+   observer.next('grand');
+   throw 'catch me!';
+   observer.next('wonderful');
+});
 
-let yin = Rx.Observable.of('peanut butter', 'wine', 'rainbows');
-let yang = Rx.Observable.of('jelly', 'cheese', 'unicorns');
+observable.catch(err => print(`Error caught: ${err}`)).subscribe(val => console.log(val));
 
-yang = yang.delay(2000);
-
-const combo = Rx.Observable.forkJoin(yin, yang);
-
-combo.subscribe(arr => console.log(arr));
-
-// wait 2 seconds...
-// rainbows, unicorns
+// good
+// great
+// grand
+// Error caught: catch me!
