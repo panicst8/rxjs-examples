f40217488bfd218cc9e7c3ef7a94393850db0bec
Chris Milford <panicst8@Chriss-MacBook-Pro.local>
2017-11-05 22:37:13 -0600

    example 22

--- lessons.js
+++ lessons.js
@@ -1,7 +1,18 @@
-// Example 21 -- TakeWhile - Get values while the conditions are right
+// Example 22 -- Buffer - Collect values, then emit as Array
 
-const names = Rx.Observable.of('Sharon', 'Sue', 'Sally', 'Steve');
-names
-   .takeWhile(name => name != 'Sally')
-   .finally(() => console.log('Complete! I found Sally'))
-   .subscribe(i => console.log(i));
+// Buffer is similar to debounce and throttle, but rather than
+// filtering out data, it accumulates it into an array, then emits
+// the array.
+
+const mouseEvents = Rx.Observable.fromEvent(document, 'mousemove');
+
+mouseEvents
+   .map(e => e.timeStamp.toFixed(0))
+   .bufferTime(1000)
+   .subscribe(batch => console.log(batch));
+// second 1 - [...values]
+// second 2 - [...values]
+
+mouseEvents.bufferCount(5).subscribe(batch => console.log(batch));
+// [...values] length 5
+// [...values] length 5
